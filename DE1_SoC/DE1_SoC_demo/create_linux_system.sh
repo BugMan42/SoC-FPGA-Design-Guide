d34cedafc9b1c8842086ca9cdce2887f88f6df13
Gulbo <and.gulberti@gmail.com>
2017-06-04 18:07:14 +0200

    added forgotten connection

--- DE1_SoC/DE1_SoC_demo/create_linux_system.sh
+++ DE1_SoC/DE1_SoC_demo/create_linux_system.sh
@@ -492,11 +492,11 @@ set -e
 mkdir -p "${sdcard_a2_dir}"
 mkdir -p "${sdcard_fat32_dir}"
 
-#compile_quartus_project
-#compile_preloader
-#compile_uboot
-#compile_linux
-#create_rootfs
+compile_quartus_project
+compile_preloader
+compile_uboot
+compile_linux
+create_rootfs
 
 # Write sdcard if it exists
 if [ -z "${sdcard_dev}" ]; then
